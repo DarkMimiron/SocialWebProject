42cc15aaae1414b43dc7c619ac05a7400b926dce
JorgeMartinezRamirez <ramirez_0157@hotmail.com>
2017-07-03 18:58:05 -0500

    Creating views and templates for groups app

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequieredMixin,
+                                        PermissionRequieredMixin)
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 # Create your views here.
+class CreateGroup(LoginRequieredMixin, generic.CreateView):
+    fields = ("name", "description")
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
